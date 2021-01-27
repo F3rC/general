a6b5a374869707639f04591d327c62bde5fae617
Fernando Corrales <fcorralesh@gmail.com>
2021-01-27 11:50:30 -0600

    Added script for blog post "Getting VMKernel Details"

---
+++ fercorrales.com_blog_code/Getting_VMKernel_Adapter_Details.ps1
@@ -0,0 +1,24 @@
+﻿$vHosts = Get-VMHost | Sort-Object Name
+
+foreach ($vHost in $vHosts) {
+    $VMHost = $vHost.Name
+    $vmks = $vHost | Get-VMHostNetwork | Select-Object -ExpandProperty VirtualNic | Sort-Object Name
+
+    foreach ($vmk in $vmks) {
+        $VmkObjct = [pscustomobject] @{'VMHost' = $vmk.VMHost
+                                  'DeviceName' = $vmk.DeviceName
+                                  'IP' = $vmk.IP
+                                  'DhcpEnabled' = $vmk.DhcpEnabled
+                                  'SubnetMask' = $vmk.SubnetMask
+                                  'Mac' = $vmk.Mac
+                                  'VMotionEnabled' = $vmk.VMotionEnabled
+                                  'ManagementTrafficEnabled' = $vmk.ManagementTrafficEnabled
+                                  'MTU' = $vmk.Mtu}
+    
+    $VmkObjct | Export-Csv -Path ~\Documents\vmk.csv -Append -NoTypeInformation
+    
+    Write-Output $VmkObjct
+
+    }
+    
+}
