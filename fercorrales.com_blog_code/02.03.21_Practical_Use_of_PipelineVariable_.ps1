5513b5854a0c65a68f9b08dcf96747ccc6b2bea7
Fernando Corrales <fcorralesh@gmail.com>
2021-02-03 22:09:09 -0600

    Script for PipelineVariable post added

---
+++ fercorrales.com_blog_code/02.03.21_Practical_Use_of_PipelineVariable_.ps1
@@ -0,0 +1,18 @@
+﻿<# Code for blog post "PowerShell: Practical Use of PipelineVariable Common Parameter"
+https://fercorrales.com/powershell-practical-use-of-pipelinevariable-common-parameter #>
+
+<#First command#> Get-ADUser -Filter {Department -eq 'Technical Support'} -Properties EmailAddress -PipelineVariable User |
+<#Second command#>Get-ADPrincipalGroupMembership |
+<#Third command#> ForEach-Object -Process {
+    $ADObj = [PSCustomObject] @{'LastName' = $User.Surname
+                                'FirstName' = $User.GivenName
+                                'SamAccountName' = $User.SamAccountName
+                                'ADUserSID' = $User.SID
+                                'EmailAddress' = $User.EmailAddress
+                                'GroupName' = $_.Name
+                                'GroupCategory' = $_.GroupCategory
+                                'ADGroupSID' = $_.SID}
+ 
+    Export-Csv -InputObject $ADObj -Path ~\Documents\ADUser_Group_Details.csv -Append -NoTypeInformation
+    
+}
